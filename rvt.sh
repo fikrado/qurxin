bdb074b24bf255ae1433d0a58c426f642e5414ef
yahye abdirahman <68783841+fikrado@users.noreply.github.com>
2020-09-24 19:02:10 +0300

    Update rvt.sh

--- rvt.sh
+++ rvt.sh
@@ -3,6 +3,6 @@ cd
 cd ..
 cd usr/etc
 rm bash.bashrc
-cd $HOME/jarvis-welcome/Revert
+cd $HOME/qurxin/Revert
 mv bash.bashrc $PREFIC/etc
-python $HOME/jarvis-welcome/Revert/Thanks.py
+python $HOME/qurxin/Revert/Thanks.py
